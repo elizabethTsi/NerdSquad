0debd9f55c7f0b311fff70fe7293a4e3d7bb6b77
Elizabeth Tsiporkin <elizabeth.tsiporkin08@gmail.com>
2025-09-05 21:15:52 -0400

    baseProject

---
+++ my-app/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">NerdSquad!</h1>
+    </main>
+  );
+}
